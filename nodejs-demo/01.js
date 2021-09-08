06d43379f23e11d138cd59733180b55c65696e93
LittleRed09 <942171959@qq.com>
2021-09-09 00:22:44 +0800

    Nodejs fs Demo

--- nodejs-demo/01.js
+++ nodejs-demo/01.js
@@ -1,2 +1,20 @@
 const args = process.argv.slice(2)
-console.log(args)
+//console.log('args',args)
+
+// fs.open()
+const fs = require('fs')
+fs.open('./hahaha.txt', 'r', (err, fd)=>{
+    //console.log('fd', fd)
+})
+
+// fs.stat()
+fs.stat('./hahaha.txt', (err, stat) => {
+    //console.log('stat', stat)
+    console.log('isFile: ', stat.isFile())
+    console.log('size: ', stat.size)
+})
+
+// fs.statSync()
+const stat2 = fs.statSync('./hahaha.txt')
+//console.log('stat2', stat2)
+stat2.isFile()
